e6499171294d045ddb3600705e4f062f5cf02e0a
mattiasvlex <mattias.vinje@gmail.com>
2024-04-07 20:44:41 +0200

    Added TodoTask and Menu classes

---
+++ CSharpProject/Menu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    internal class Menu
+    {
+        public static void Welcome(int tasksTodo, int tasksDone) 
+        {
+            Console.WriteLine($"Welcome to Todo list\nYou have {tasksTodo} tasks to do and {tasksDone} tasks are done");
+        }
+
+        public static string GetCommand(string[] options)
+        {
+            Console.WriteLine("\nPick an option: ");
+            for (int i = 0; i < options.Length; i++)
+            {
+                Console.WriteLine($"({i+1}) {options[i]}");
+            }
+            
+            string? command = Console.ReadLine();
+            return command ?? "";
+        }
+
+        public static string GetTitle()
+        {
+            Console.Write("Enter title: ");
+            string? title = Console.ReadLine();
+            return title ?? "";
+        }
+
+        public static string GetProject()
+        {
+            Console.Write("Enter project name: ");
+            string? project = Console.ReadLine();
+            return project ?? "";
+        }
+
+        public static void PrintError(string error)
+        {
+            Console.WriteLine("Invalid input: " + error);
+        }
+
+        public static void PrintAll(List<TodoTask> tasks)
+        {
+            Console.WriteLine();
+            foreach (var t in tasks)
+            {
+                Console.WriteLine(t);
+            }
+        }
+    }
+}
---
+++ CSharpProject/TodoTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    internal class TodoTask
+    {
+        public string Title { get; set; }
+        public DateOnly Date { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public string Project { get; set; }
+
+        public TodoTask(string title, DateOnly date, string project)
+        {
+            Title = title;
+            Date = date;
+            Project = project;
+        }
+
+        private string Completed()
+        {
+            return (this.IsCompleted) ? "Yes" : "No";
+        }
+
+        public override string ToString()
+        {
+            return this.Title.PadRight(15) + this.Project.PadRight(15) + this.Date.ToString().PadRight(15) + this.Completed();
+        }
+
+		public string ToCSV()
+		{
+			return this.Title + "," + this.Project + "," + this.Date.ToString() + "," + this.Completed();
+		}
+    }
+}
